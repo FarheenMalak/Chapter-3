7c7157a19f875bec56e1deb3d8803fb7a15e7301
FarheenMalak <115576023+FarheenMalak@users.noreply.github.com>
2024-08-12 11:26:14 +0500

    Update numbers-variables.js

--- numbers-variables.js
+++ numbers-variables.js
@@ -1,3 +1,4 @@
+
 // #3 VARIABLES FOR NUMBERS
 
 var age = 17;
@@ -7,11 +8,11 @@ var visitors = 14;
 alert(" You have visited this site " + visitors + " times ");
 
 var birthYear = 2005;
-document.write(' My Birth Year is ' +birthYear+ '<br/> ' Data type of my declared variable is number ');
+document.write(' My Birth Year is ' +birthYear+ '<br/>  Data type of my declared variable is number ');
 
 var vistorName = " Farheen Malak ";
 var quantity = "4";
-var product = "scrafs";
+var product = "scarfs";
 document.write(visitorName + " Ordered " + quantity + " " + product + " On XYZ Cloth Store ");
 
 
